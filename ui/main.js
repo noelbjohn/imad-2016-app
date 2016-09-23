76be1a1c2008bea5aaea474c7c4ab8bad34f9a9c
Noel B John <noelbjohn@yahoo.com>
2016-09-23 20:03:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="Hello World";
 var img=document.getElementById("madi");
-img.onClick=function(){
+img.onclick=function(){
     img.style.marginLeft='100px';
 }
