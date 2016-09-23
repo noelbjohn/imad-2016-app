2d4980d8399203e95196c1803486d2c37b6fb0e7
noelbjohn <noelbjohn@gmail.com>
2016-09-23 19:28:12 +0530

    Changed contents of server.js and removed article-one,article-two and article-three file from ui

--- server.js
+++ server.js
@@ -5,20 +5,80 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    'article-one':{
+        title: 'Article One !Noel B John',
+        heading: 'Article One',
+        date: 'Sept 3, 2016',
+        content:`<p>
+                    This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page.This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page.
+                </p>
+                <p>
+                    This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page.This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page.
+                </p>
+                <p>
+                    This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page.This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page. This is the contents of article one. This is a sample page.
+                </p>`
+    },
+    'article-two':{
+        title: 'Article Two !Noel B John',
+        heading: 'Article Two',
+        date: 'Sept 5, 2016',
+        content: `<p>
+                    This is the contents of article two. This is a sample page. This is the contents of article two. This is a sample page. This is the contents of article two. This is a sample page. This is the contents of article two. This is a sample page. This is the contents of article two. This is a sample page. This is the contents of article two. This is a sample page.
+                </p>`
+    },
+    'article-three':{
+        title: 'Article Three !Noel B John',
+        heading: 'Article Three',
+        date: 'Sept 15, 2016',
+        content: `<p>
+                    This is the contents of article three. This is a sample page.
+                </p>`
+    }
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var HTMLTemplate=`<!doctype html>
+                        <head>
+                            <title>
+                                ${title}
+                            </title>
+                            <meta name="viewport" content="width=device-width, initial-scale=1" />
+                            <link href="/ui/style.css" rel="stylesheet" />
+                        </head>
+                        <body>
+                            <div class="container">
+                                <div>
+                                    <a href="/">Home</a>
+                                </div>
+                                <hr/>
+                                <h1>
+                                    ${heading}
+                                </h1>
+                                <div>
+                                    ${date}
+                                </div>
+                                <div>
+                                    ${content}
+                                </div>
+                            </div>
+                        </body>
+                    </html>`;
+    return HTMLTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+  var articleName=req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
