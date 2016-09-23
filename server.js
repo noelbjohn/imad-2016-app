cc42e7449c366d7fde8ac5cb0c83bd9c82ff4802
Noel B John <noelbjohn@yahoo.com>
2016-09-23 20:53:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1;
+  res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
   var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -83,12 +89,6 @@ app.get('/:articleName', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-var counter = 0;
-app.get('/counter', function (req, res) {
-  counter = counter + 1;
-  res.send(counter.toString());
 });
 
 app.get('/ui/madi.png', function (req, res) {
