face6bab24aec58f71bd84547be3f18a4775ded9
Noel B John <noelbjohn@yahoo.com>
2016-09-26 09:10:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,5 +18,11 @@ var nameInput=document.getElementById("name");
 var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
-    
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<name.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    ul=document.getElementById("namelist");
+    ul.innerHTMl=list;
 };
