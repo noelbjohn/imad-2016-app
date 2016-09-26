824d4045c6bbffcbd3c7306f08a6092e3ea0c8e3
Noel B John <noelbjohn@yahoo.com>
2016-09-26 10:07:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,11 +18,21 @@ var nameInput=document.getElementById("name");
 var name=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    ul=document.getElementById("namelist");
-    ul.innerHTML=list;
+    var httprequest=new XMLHttpRequest();
+    httprequest.onreadystatechange=function(){
+      if(httprequest.readyState===XMLHttpRequest.DONE){
+        if(httprequest.status===200){
+            var names=httprequest.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for(var i=0;i<names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+            }
+            ul=document.getElementById("namelist");
+            ul.innerHTML=list;
+        }
+      }
+    };
+    httprequest.open('GET','http://noelbjohn.imad.hasura-app.io/counter',true);
+    httprequest.send(null);
 };
