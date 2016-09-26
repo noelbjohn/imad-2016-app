f75c9b7ef1e5cb65062da330a83f02b391e9cd06
Noel B John <noelbjohn@yahoo.com>
2016-09-26 08:55:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
-var nemes=[];
+var names=[];
 app.get('/submit-name/:name', function (req, res) {
   var name=req.params.name;
   names.push(name);
