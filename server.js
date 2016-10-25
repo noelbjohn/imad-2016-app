70f45ba4ff726206edeb285f272f5baac17d52a5
Noel B John <noelbjohn@yahoo.com>
2016-10-25 18:13:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,9 +120,17 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/:articleName', function (req, res) {
-  var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+  pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'",function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else if(result.rows.length === 0){
+            res.status(404).send('Article not found');
+        }else{
+            var articleData = result.rows[0];
+            res.send(createTemplate(articles[articleData]));
+        }
+    });
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
